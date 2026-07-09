dcaa7e1548c2fd154e9ca01fbbbd24837da0461a
Parshvi02 <parshvi02@gmail.com>
2026-07-09 22:31:25 +0530

    read group

--- backend/Parshvi/routes/groupRoute.js
+++ backend/Parshvi/routes/groupRoute.js
@@ -1,7 +1,11 @@
 const express = require("express");
 const router = express.Router();
 
-const create = require("../controllers/group/create");
+const groupController = require("../controllers/group");
 
-router.post("/",create);
+router.post("/", groupController.create);
+router.get("/", groupController.read);
+// router.put("/:id", notesController.update);
+// router.delete("/:id", notesController.delete);
+
 module.exports = router;
